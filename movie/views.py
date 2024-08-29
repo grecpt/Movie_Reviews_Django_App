5b3e2bdca23e732825a6e567877bc475463849cd
Rex Makusia <grecpt@gmail.com>
2024-08-30 00:01:07 +1000

    Movie_Reviews_Django_App: Update the application views

--- movie/views.py
+++ movie/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name':'Rex Makusia'})
-
+    searchTerm =request.GET.get('searchMovie')
+    return render(request, 'home.html', 
+                  {'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
