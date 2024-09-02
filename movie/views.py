1c3a348ad280bb72203c500d056188b029e22029
Rex Makusia <grecpt@gmail.com>
2024-09-02 12:25:28 +1000

    Movie_Reviews_Django_App: Update the Project views

--- movie/views.py
+++ movie/views.py
@@ -12,3 +12,6 @@ def home(request):
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
 
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
