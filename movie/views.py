2303316f3cc46a7d8e730db613a063ca22961a9f
Rex Makusia <grecpt@gmail.com>
2024-09-26 20:54:34 +1000

    Movie_Reviews_Django_App : Refactor assignment of application views

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    searchTerm =request.GET.get('searchMovie')
+    searchTerm = request.GET.get('searchMovie')
     return render(request, 'home.html', 
                   {'searchTerm':searchTerm})
 
