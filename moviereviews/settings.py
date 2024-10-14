23ea171ae3a18ff770e746cae72bd9b22d18ecaf
Rex Makusia <grecpt@gmail.com>
2024-10-14 14:59:45 +1000

    Update the base template

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL  = '/media/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'moviereviews/static/',
 ]
